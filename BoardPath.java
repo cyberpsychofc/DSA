49a2186914a6ca90455689f2307e6dd14b252d6b
Om Aryan <79821189+cyberpsychofc@users.noreply.github.com>
2024-07-19 11:15:00 +0530

    Add files via upload
    backtracking - boardPath

---
+++ BoardPath.java
@@ -0,0 +1,21 @@
+package test;
+
+public class BoardPath {
+	public static void Boardpath(boolean arr[], int qpsf, int totq, String ans) {
+		if(qpsf == totq) {
+			System.out.println(ans);
+			return;
+		}
+		for(int i=0; i< arr.length; i++) {
+			if(arr[i]==false) {
+				arr[i] = true;
+				Boardpath(arr,qpsf+1,totq, "q" + qpsf + "b" + i + ans);
+				arr[i] = false;
+			}
+		}
+	}
+	public static void main(String arg[]) {
+		boolean arr[] = new boolean[4];
+		Boardpath(arr,0,2,"");
+	}
+}
