02ecdccd75220bcf6b0f0b9f23910b9cb34b1c4e
Om Arryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-06-20 13:03:31 +0530

    Time: 9 ms (11.04%) | Memory: 42.9 MB (22.28%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -1,17 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> answer = new ArrayList<String>();
-        if (root != null)
-            searchBT(root, "", answer);
-        return answer;
-    }
+        List<String> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
 
-    private void searchBT(TreeNode root, String path, List<String> answer) {
-        if (root.left == null && root.right == null)
-            answer.add(path + root.val);
-        if (root.left != null)
-            searchBT(root.left, path + root.val + "->", answer);
-        if (root.right != null)
-            searchBT(root.right, path + root.val + "->", answer);
+        Stack<Pair<TreeNode, String>> stack = new Stack<>();
+        stack.push(new Pair<>(root, String.valueOf(root.val)));
+
+        while (!stack.isEmpty()){
+            Pair <TreeNode, String> node = stack.pop();
+
+            TreeNode pointer = node.getKey();
+            String path = node.getValue();
+
+            if (pointer.left == null && pointer.right == null)
+                ans.add(path);
+
+            if (pointer.left != null)
+                stack.push(new Pair<>(pointer.left, path + "->" + pointer.left.val));
+            if (pointer.right != null)
+                stack.push(new Pair<>(pointer.right, path + "->" + pointer.right.val));
+        }
+        return ans;
     }
 }
