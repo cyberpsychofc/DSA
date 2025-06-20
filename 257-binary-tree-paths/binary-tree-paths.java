20d229fd9fac7bdd230bfb91c718ae6b25a31c78
Om Arryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-06-20 13:01:40 +0530

    Time: 9 ms (11.04%) | Memory: 42.6 MB (75.09%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -1,41 +1,22 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> ans = new ArrayList<>();
-        if (root == null)
-            return ans;
-
-        Stack<Pair<TreeNode, String>> stack = new Stack<>();
-        stack.push(new Pair<>(root, String.valueOf(root.val)));
-
-        while (!stack.isEmpty()){
-            Pair <TreeNode, String> node = stack.pop();
-
-            TreeNode pointer = node.getKey();
-            String path = node.getValue();
-
-            if (pointer.left == null && pointer.right == null)
-                ans.add(path);
-
-            if (pointer.left != null)
-                stack.push(new Pair<>(pointer.left, path + "->" + pointer.left.val));
-            if (pointer.right != null)
-                stack.push(new Pair<>(pointer.right, path + "->" + pointer.right.val));
+        List<String> paths = new ArrayList<>();
+        if (root != null) {
+            dfs(root, "", paths);
         }
-        return ans;
+        return paths;
+    }
+    
+    private void dfs(TreeNode node, String path, List<String> paths) {
+        if (node.left == null && node.right == null) {
+            paths.add(path + node.val);
+            return;
+        }
+        if (node.left != null) {
+            dfs(node.left, path + node.val + "->", paths);
+        }
+        if (node.right != null) {
+            dfs(node.right, path + node.val + "->", paths);
+        }
     }
 }
