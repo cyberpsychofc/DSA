92a16ba1182703699e41c6801c9f950f734e56cd
Om Arryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-06-20 13:03:07 +0530

    Time: 7 ms (60.18%) | Memory: 42.9 MB (35.39%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.java
+++ 257-binary-tree-paths/binary-tree-paths.java
@@ -1,22 +1,17 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
-        List<String> paths = new ArrayList<>();
-        if (root != null) {
-            dfs(root, "", paths);
-        }
-        return paths;
+        List<String> answer = new ArrayList<String>();
+        if (root != null)
+            searchBT(root, "", answer);
+        return answer;
     }
-    
-    private void dfs(TreeNode node, String path, List<String> paths) {
-        if (node.left == null && node.right == null) {
-            paths.add(path + node.val);
-            return;
-        }
-        if (node.left != null) {
-            dfs(node.left, path + node.val + "->", paths);
-        }
-        if (node.right != null) {
-            dfs(node.right, path + node.val + "->", paths);
-        }
+
+    private void searchBT(TreeNode root, String path, List<String> answer) {
+        if (root.left == null && root.right == null)
+            answer.add(path + root.val);
+        if (root.left != null)
+            searchBT(root.left, path + root.val + "->", answer);
+        if (root.right != null)
+            searchBT(root.right, path + root.val + "->", answer);
     }
 }
