b99e9685b9f0c733c9dbc8957a1177a4882fa084
Om Arryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-06-21 15:36:43 +0530

    Time: 5 ms (95.04%) | Memory: 45.7 MB (52.48%) - LeetSync

--- 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
+++ 3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.java
@@ -1,21 +1,36 @@
 class Solution {
     public int minimumDeletions(String word, int k) {
-        Map<Character, Integer> cnt = new HashMap<>();
-        for (char ch : word.toCharArray()) {
-            cnt.put(ch, cnt.getOrDefault(ch, 0) + 1);
+        int[] freq = new int[26];
+        for (char c : word.toCharArray()) {
+            freq[c - 'a']++;
         }
-        int res = word.length();
-        for (int a : cnt.values()) {
-            int deleted = 0;
-            for (int b : cnt.values()) {
-                if (a > b) {
-                    deleted += b;
-                } else if (b > a + k) {
-                    deleted += b - (a + k);
-                }
+
+        int[] updated = new int[26];
+        int size = 0;
+        for (int f : freq) {
+            if (f > 0) updated[size++] = f;
+        }
+
+        Arrays.sort(updated, 0, size);
+
+        int min = 100000, totalSum = word.length(), deletedLeftSum = 0, sum = 0, j = 0;
+        for (int i = 0; i < size; i++) {
+            int from = updated[i], to = from + k;
+            while (j < size && updated[j] <= to) {
+                sum += updated[j++];
             }
-            res = Math.min(res, deleted);
+
+            int left = size - j;
+            int leftSum = totalSum - sum;
+            int required = left * to;
+            int deleted = leftSum - required;
+
+            min = Math.min(min, deleted + deletedLeftSum);
+
+            totalSum -= updated[i];
+            deletedLeftSum += updated[i];
+            sum -= updated[i];
         }
-        return res;
+        return min;
     }
 }
