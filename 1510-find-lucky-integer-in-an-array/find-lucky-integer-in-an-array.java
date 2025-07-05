4989abbfe25f6d9e1a865b5796a432b2f8e08fd3
Aryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-07-05 11:47:29 +0530

    Time: 5 ms (72.10%) | Memory: 43.1 MB (93.60%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -8,8 +8,8 @@ class Solution {
 
         int max = -1;
         for (Map.Entry<Integer, Integer> entry : map.entrySet()){
-            if(entry.getKey() == entry.getValue())  
-                max = Math.max(max, entry.getKey());
+            if(entry.getKey() == entry.getValue() && entry.getKey() > max)  
+                max = entry.getKey();
         }
 
         return max;
