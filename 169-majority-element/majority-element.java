41b2f39d3cac8ba25ba147993f75b49a50286d46
Aryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-07-07 21:22:55 +0530

    Time: 1 ms (99.85%) | Memory: 53.1 MB (59.79%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,12 +1,13 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        int num = 0, count = 0;
 
+        int num = 0, count = 0;
         for (int x : nums){
             if (count == 0)
                 num = x;
-            count += (x == num ? 1 : -1);
+            // check if it's the same number, else decrease count by 1
+            count += (x == num) ? 1 : -1;
         }
         count = 0;
         for (int x : nums){
@@ -15,6 +16,7 @@ class Solution {
         }
         if (count > n / 2)
             return num;
+
         return -1;
     }
 }
