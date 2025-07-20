a13ce612998865cce9d024410c09bd85ac4e6a11
Aryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-07-20 10:53:08 +0530

    Time: 1 ms (79.56%) | Memory: 41.8 MB (10.16%) - LeetSync

---
+++ 290-word-pattern/word-pattern.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean wordPattern(String pattern, String s) {
+        String[] words = s.split(" ");
+        if (pattern.length() != words.length) {
+            return false;
+        }
+        Map<Character, String> map = new HashMap<>();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            if (map.containsKey(pattern.charAt(i)) && map.get(pattern.charAt(i)).equals(words[i]))
+                continue;
+            else if (map.containsKey(pattern.charAt(i)) && !(map.get(pattern.charAt(i)).equals(words[i])) || !map.containsValue(pattern.charAt(i)) && map.containsValue(words[i])) {
+                return false;
+                }
+            else
+                map.put(pattern.charAt(i), words[i]);
+        }
+        return true;
+    }
+}
