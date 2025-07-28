83f065367ac5e3a68f45c4891522fe6efbc338b3
Aryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-07-28 08:06:11 +0530

    Time: 2 ms (57.79%) | Memory: 45 MB (40.59%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] transformArray(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+                nums[i] = nums[i] & 1;
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+}
