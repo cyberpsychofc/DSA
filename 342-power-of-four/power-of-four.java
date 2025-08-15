cd9b3ccf777e405ff8464e3ce1b5b4c100551b30
Aryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-08-15 10:51:44 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (60.08%) - LeetSync

---
+++ 342-power-of-four/power-of-four.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        double power = Math.log(n) / Math.log(4);
+        if (power == 1 && n % 4 != 0)
+            return false;
+        return true ? power % 1 == 0.0 : false; 
+    }
+}
