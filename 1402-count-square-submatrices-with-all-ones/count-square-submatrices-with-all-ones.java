db86e3c9328e7ee256e38c2e5c36a50c21035700
Aryan <79821189+cyberpsychofc@users.noreply.github.com>
2025-08-20 09:43:00 +0530

    Time: 5 ms (98.48%) | Memory: 55.1 MB (53.73%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.java
@@ -6,14 +6,10 @@ class Solution {
         // right bottom, since we are traversing from left to right and top to bottom (we need a square)
 
         // take min. of up, left and diagonal
-        for (int i = 0; i < m; i++){
+        for (int i = 0; i < m; i++)
             dp[i][0] = matrix[i][0];
-            sum += dp[i][0];
-        }
-        for (int j = 1; j < n; j++){
+        for (int j = 0; j < n; j++)
             dp[0][j] = matrix[0][j];
-            sum += dp[0][j];
-        }
 
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
@@ -21,6 +17,11 @@ class Solution {
                     dp[i][j] = 0;
                 else
                     dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+            }
+        }
+        // add the DP table
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
                 sum += dp[i][j];
             }
         }
